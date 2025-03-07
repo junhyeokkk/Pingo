3a86bdfe8909edd52beaa59d605a217ba9bb6ecf
junhyeokkk <104557505+junhyeokkk@users.noreply.github.com>
2025-03-07 15:21:58 +0900

    Update KafkaTopics.java

--- back/src/main/java/com/pingo/util/KafkaTopics.java
+++ back/src/main/java/com/pingo/util/KafkaTopics.java
@@ -2,6 +2,5 @@ package com.pingo.util;
 
 public class KafkaTopics {
     public static final String SWIPE_EVENTS = "swipe-events-1";  // 스와이프 이벤트
-    public static final String MATCH_EVENTS = "match-events";  // 매칭 이벤트
     public static final String SWIPE_EVENTS_DLQ = "swipe-events-1-dlq"; // 스와이프 이벤트 실패
 }
